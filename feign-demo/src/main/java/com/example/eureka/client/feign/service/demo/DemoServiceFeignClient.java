4ee92be1d36294d34ec3f91242408a8804a8e545
Lin Fan <flin@thoughtworks.com>
2017-05-10 20:33:52 +0800

    remove useless base class

--- feign-demo/src/main/java/com/example/eureka/client/feign/service/demo/DemoServiceFeignClient.java
+++ feign-demo/src/main/java/com/example/eureka/client/feign/service/demo/DemoServiceFeignClient.java
@@ -1,8 +1,13 @@
 package com.example.eureka.client.feign.service.demo;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @FeignClient(value = "eureka-client", fallback = DemoServiceFeignClientFallback.class)
-public interface DemoServiceFeignClient extends DemoService {
-	
+public interface DemoServiceFeignClient {
+    @RequestMapping(value = "/api/hello", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public String hello();
+
 }
