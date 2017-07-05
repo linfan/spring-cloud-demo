39c18adecd1bd01ccc2575078919bd647c660d0a
Lin Fan <flin@thoughtworks.com>
2017-07-05 15:03:53 +0800

    remove deprecated implement

--- eureka-client/src/main/java/com/demo/eureka/client/controller/DemoController.java
+++ eureka-client/src/main/java/com/demo/eureka/client/controller/DemoController.java
@@ -1,6 +1,7 @@
 package com.demo.eureka.client.controller;
 
 import com.demo.eureka.client.vo.User;
+import com.google.common.collect.ImmutableMap;
 import com.netflix.appinfo.InstanceInfo;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.netflix.eureka.serviceregistry.EurekaRegistration;
 import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
+import java.util.Map;
 
 /**
  * ApiOperation和ApiParam可理解为API说明
@@ -22,26 +24,26 @@ public class DemoController {
 	@Value("${demo.env}")
 	private String env;
 
-	@ApiOperation(value="演示从环境上下文和配置文件读取信息")
+	@ApiOperation(value="演示从配置文件读取信息")
     @RequestMapping(value = "", method = RequestMethod.GET)
-    public String hello() {
-        InstanceInfo instanceInfo = registration.getApplicationInfoManager().getInfo();
-        return "Hello! It's " + env  + ". " + registration.getServiceId() +
-                ":" + instanceInfo.getHostName() + ":" + instanceInfo.getPort();
+    public Map<String, String> hello() {
+        return ImmutableMap.of("hello", env);
     }
 	
-	@ApiOperation(value="演示GET请求参数")
+	@ApiOperation(value="演示从环境上下文读取信息")
     @RequestMapping(value = "/{name}", method = RequestMethod.GET)
-    public String hello(@ApiParam(required=true, name="name", value="用户名") @PathVariable String name) {
-        return "Hello! " + name;
+    public Map<String, String> hello(@ApiParam(required=true, name="name", value="用户名") @PathVariable String name) {
+        InstanceInfo instanceInfo = registration.getApplicationInfoManager().getInfo();
+        return ImmutableMap.of("name", registration.getServiceId(),
+                "address", instanceInfo.getHostName() + ":" + instanceInfo.getPort());
     }
 	
-	@ApiOperation(value="演示POST请求实体")
+	@ApiOperation(value="演示处理POST请求")
     @RequestMapping(value = "/{greeting}", method = RequestMethod.POST)
-    public String hello(
+    public Map<String, String> hello(
     		@ApiParam(required=true, name="greeting", value="自定义问候语") @PathVariable String greeting, 
     		@RequestBody User user) {
-        return greeting + "! " + user.getUserName();
+        return ImmutableMap.of(greeting, user.getName());
     }
 	
 	/**
